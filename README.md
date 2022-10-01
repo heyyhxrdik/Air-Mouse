8be47d834079d776ac47214dbf3f16ea891c7575
heyyhxrdik <pseudopythonic@gmail.com>
2022-10-01 10:45:57 +0530

    first commit 🎊

---
+++ README.md
@@ -0,0 +1,53 @@
+
+# _Air Mouse_
+
+_Air Mouse allows you to control your mouse with the gestures of your hand! In the current state, you can move the mouse and drag 'n drop items._
+    
+## _Run Locally_
+
+_Clone the project_
+
+```bash
+  git clone https://github.com/heyyhxrdik/Air-Mouse
+```
+
+_Go to the project directory_
+
+```bash
+  cd Air-Mouse
+```
+
+_Install dependencies_
+
+```bash
+  pip install requirements.txt
+```
+
+_Run the app_
+
+```bash
+  python src/main.py
+```
+
+
+## _Tech Stack_
+
+_**Client:** Python OpenCV_
+
+_**Server:** Python OpenCV_
+
+
+## _Lessons Learned_
+
+_I learned about computer vision. I also came to know how to integrate different ML models to detect the hands, fingers and their landmarks!_
+
+
+## _License_
+
+_This project is [MIT](https://github.com/heyyhxrdik/Air-Mouse/blob/master/LICENSE) licensed._
+
+
+## _Support_
+
+_For support, email pseudopythonic@gmail.com or connect with me on Linkedin._
+
---
+++ src/main.py
@@ -0,0 +1,89 @@
+# Importing required modules
+from imutils.video import WebcamVideoStream
+import imutils
+import cv2
+import time
+import HandDetector
+import pyautogui
+from pynput.mouse import Controller
+
+
+from MouseMove import MouseMove
+from MouseClick import MouseClick
+from MouseDragDrop import MouseDragDrop
+
+# Get the controller object
+mouse = Controller()
+# Initialise the camera object
+vs = WebcamVideoStream(src=0).start()
+
+# Initialise hand detection
+detector = HandDetector.Detector()
+# Get the screen size
+screen = pyautogui.size()
+
+# Required variables
+pTime = 0
+smooth = 5
+previousLocation = [0, 0]
+currentLocation = [0, 0]
+
+while True:
+    # Read the frame
+    frame = vs.read()
+    # Resize the frame's width to 600px
+    frame = imutils.resize(frame, width=1000)
+    # Flip the frame to selfie mode
+    frame = cv2.flip(frame, 1)
+
+    # Get the height, width and channel from the frame
+    height, width, channel = frame.shape
+
+    # Get FPS
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    # Show FPS
+    cv2.putText(frame, f"FPS: {int(fps)}",
+                (40, 40), cv2.FONT_HERSHEY_PLAIN, 2, (10, 0, 18), 2)
+
+    # Show instruction to quit the window
+    cv2.putText(frame, f"Press 'q' to exit",
+                (40, 80), cv2.FONT_HERSHEY_PLAIN, 2, (10, 0, 18), 2)
+
+    # Finds index of each landmark on the recognised hand
+    landmarkList = detector.findIndex(frame)
+    # Get which fingers are raised up
+    fingers = detector.fingerIsUp()
+
+    # Make the movable area
+    cv2.rectangle(frame, (100, 30), (width-100, height-200), (0, 255, 0), 2)
+
+    if(len(landmarkList) != 0):
+        # Make the cursor go to the extreme corner
+        pyautogui.FAILSAFE = False
+
+        # If the index finger is only raised up
+        if(fingers[0] == 0 and fingers[1] == 1 and fingers[2] == 0):
+            MouseMove(frame, landmarkList, screen, mouse)
+
+        # If the index and middle finger is both up
+        if(fingers[0] == 0 and fingers[1] == 1 and fingers[2] == 1):
+            MouseClick(frame, landmarkList, mouse)
+
+        # If the thumb and index finger is both up
+        if(fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 0):
+            MouseDragDrop(frame, landmarkList, mouse)
+
+    # Show the frame in a window
+    cv2.imshow("Air Mouse", frame)
+
+    # If the 'q' key is pressed then exit the window
+    if(cv2.waitKey(5) & 0xFF == ord('q')):
+        break
+
+# destroy all the windows
+cv2.destroyAllWindows()
+# Stop recording
+vs.stop()
